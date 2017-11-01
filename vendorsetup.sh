c7a9d7c83766714d6fad5dde584a02b29b3b04da
zac6ix <nanaquameallday@gmail.com>
2017-11-01 18:15:31 +0000

    use userdebug for now

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
 # Use this to add HT16 to CM's lunch command menu
-for var in eng user userdebug; do
-  add_lunch_combo lineage_HT16-$var
+for var in userdebug; do
+  add_lunch_combo lineage_X510-$var
 done
